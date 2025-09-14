2c0ee8a6886e0c899196a5cef449b798d1530d15
yy31104 <147130934+yy31104@users.noreply.github.com>
2025-09-15 01:25:42 +0200

    Update save_load.h

--- save_load.h
+++ save_load.h
@@ -5,8 +5,8 @@
 #include "board.h"
 #include "player.h"
 
-/* 把整个游戏进度写到文件（棋盘、玩家、分数、状态） */
+
 int save_game(const char* filename, const struct GameState* gs, const struct players* pl);
 
-/* 从文件读取整个游戏进度（会按文件内容重新分配并填充棋盘） */
 int load_game(const char* filename, struct GameState* gs, struct players* pl);
+
