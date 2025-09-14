3a6e088e6c612d983ed6af07eaf16227d9e2354c
yy31104 <147130934+yy31104@users.noreply.github.com>
2025-09-15 01:25:19 +0200

    Update player.h

--- player.h
+++ player.h
@@ -6,23 +6,23 @@
 #include "function.h"
 #include "board.h"
 
-/* 单只企鹅 */
+
 struct Penguin {
     int     penID;
     CoordXY penguinCoords;
     int     can_make_any_move;
 };
 
-/* 单个玩家 */
+
 struct Player {
     char           name[25];
     int            score;
     int            numOfPenguinsCheckedOut;
     int            numberOfPlacedPenguins;
-    struct Penguin penguins[3];   // 你项目里最多 3 只
+    struct Penguin penguins[3];   
 };
 
-/* 玩家集合 + 全局玩家状态 */
+
 struct players {
     struct Player playr[2];
     int currentPlayer;
@@ -30,15 +30,16 @@ struct players {
     int someoneCantMove;
 };
 
-/* 游戏整体状态（含棋盘） */
+
 struct GameState {
     Board board;
     int hasPlacementPhaseEnded;
     int placingPenguinsOnlyOnOne;
 };
 
-/* 其余函数声明 */
+
 void change_current_player(struct players* player);
 void set_current_player(struct players* player);
+
 
 #endif
